76d458fc8fd9123b00363c155efe10cb63525bb8
ReshmaR29 <reshmareddy2211@gmail.com>
2025-07-02 18:44:27 +0530

    feat: add comment feature to Post

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,8 @@ from django.views.generic import ListView
 from django.core.mail import send_mail
 from .forms import EmailPostForm
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
+from .forms import CommentForm
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -17,7 +18,22 @@ def post_detail(request, year, month, day, post):
                                    publish__month=month,
                                    publish__day=day,
     )
-    return render(request, 'blog/post/detail.html', {'post': post})
+
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, 'blog/post/detail.html', {'post': post,
+                                                     'comments': comments,
+                                                     'new_comment': new_comment,
+                                                     'comment_form': comment_form,})
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status='published')
