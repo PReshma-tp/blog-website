237738346bf736b74ccaba3f230e6e3c7cf393c0
ReshmaR29 <reshmareddy2211@gmail.com>
2025-07-04 15:36:49 +0530

    Refactor: connecting to PostgreSql replacing SQLite

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -80,9 +80,11 @@ WSGI_APPLICATION = 'blogsite.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+ 'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog',
+        'PASSWORD': '1234',
     }
 }
 
